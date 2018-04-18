fd0cc9d9c8cfe39c894d7bf5e52ab7541aa62712
bhavanianant_lu <kid2@banant.com>
2018-04-18 04:43:07 -0400

    part one and two complete using features

--- checkpoint5/demo9.py
+++ checkpoint5/demo9.py
@@ -6,6 +6,7 @@ from part_two import *
 from collections import defaultdict
 from collections import Counter
 from copy import deepcopy
+# import pprint
 
 
 """
@@ -18,17 +19,50 @@ ideally would be able to interrupt a robot if supportive action wrong
 add more to htm data to develop heuristics for simulated feature-vector generation more readily
 """
 
-def part_one():
-	myHTM = chair_task_HTM()
-	# print chair_task_features()
+NUM_INIT_TRAINERS = 3
+
+"""
+Part 1 (new)
+Show how generate user ground truth labels to set of observations (things that have been done at end of previous time step)
+"""
+def part_one(myHTM):
 	t = generate_rand_state_seq(myHTM)
-	print t
-	prettyprint(generate_user(t))
-	pass
+	user_pref = generate_user(t) #list of tuples(size2) of tuples(?)
+	return t, user_pref
+
+"""
+Part 2 
+Initial Training Period
+"""
+def part_two(myHTM):
+	default_supp_actions = defaultdict(Counter)
+	for trainer in range(NUM_INIT_TRAINERS):
+		t = generate_rand_state_seq(myHTM)
+		trainer_pref = generate_user(t)
+		for timestep, (feats, supp_acts) in enumerate(trainer_pref):
+			default_supp_actions[(timestep,feats)][supp_acts] += 1
+	# print default_supp_actions
+	prettyprint(default_supp_actions, sort=True)
+
+
+
 
 
 def main():
-	part_one()
-	pass
+	myHTM = chair_task_HTM()
+	print "---Possible features:---"
+	print chair_task_features()
+	print "=== PART ONE ==="
+	print "Demonstration of how user ground truth labels to feature vectors are generated:"
+	t, sample_user_pref = part_one(myHTM)
+	print "Sequence of task states, for reference:" + str(t)
+	# prettyprint(sample_user_pref)
+	prettyprint(sample_user_pref, sort=True)
+	print "=== PART TWO ==="
+	print "Result of training iterations"
+	part_two(myHTM)
+	
+
+
 
 if __name__ == "__main__": main()
--- checkpoint5/utils.py
+++ checkpoint5/utils.py
@@ -1,13 +1,19 @@
 from itertools import chain, combinations
 
+
 def powerset(iterable):
 	s = list(iterable)
 	return chain.from_iterable(combinations(s,r) for r in range(1, len(s) + 1))
 
-def prettyprint(matrix):
+def prettyprint(matrix, sort=False):
 	if isinstance(matrix, dict):
-		for key in matrix.keys():
-			print(str(key) + '\t' + str(matrix[key]))
+		max_len = max([len(str(k)) for k in matrix.keys()]) + 1
+		if sort:
+			for key in sorted(matrix.keys()):
+				print str(key) + str(' ' * (max_len - len(str(key)))  + str(matrix[key]))
+		else:
+			for key in matrix.keys():
+				print str(key) + str(' ' * (max_len - len(str(key)))  + str(matrix[key]))
 		print
 	else:
 		s = [[str(e) for e in row] for row in matrix]
